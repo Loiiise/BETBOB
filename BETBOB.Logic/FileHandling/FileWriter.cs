e810f1e98aef7cd27e904fdcc18febd7c60821bb
Loïse <45357831+Loiiise@users.noreply.github.com>
2025-03-25 13:29:22 +0100

    #10 implemented filewriter

---
+++ BETBOB.Logic/FileHandling/FileWriter.cs
@@ -0,0 +1,14 @@
+﻿namespace BETBOB.Logic.FileHandling;
+
+public class FileWriter : IFileWriter
+{
+    public void WriteToFile(string filePath, string content, bool overwrite)
+    {
+        if (!overwrite && File.Exists(filePath))
+        {
+            return;
+        }
+
+        File.WriteAllText(filePath, content);
+    }
+}
