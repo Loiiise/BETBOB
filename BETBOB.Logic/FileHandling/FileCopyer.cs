c3878275ae4140592501e2d5d2a8853cdd158be2
Loïse <45357831+Loiiise@users.noreply.github.com>
2025-03-25 14:32:22 +0100

    #10 implemented follow up folecopyer

--- BETBOB.Logic/FileHandling/FileCopyer.cs
+++ BETBOB.Logic/FileHandling/FileCopyer.cs
@@ -4,4 +4,7 @@ public class FileCopyer : IFileCopyer
 {
     public void CopyFile(string sourcePath, string desinationPath, bool overwrite)
         => File.Copy(sourcePath, desinationPath, overwrite);
+
+    public void CopyFile(FileInfo sourceFile, string desinationPath, bool overwrite)
+        => sourceFile.CopyTo(desinationPath, overwrite);
 }
