f7456662c10559c2827f2d2c5c7ccf33bd82f438
Loïse <45357831+Loiiise@users.noreply.github.com>
2025-03-26 21:14:51 +0100

    #30 implemented cleanup command

---
+++ BETBOB.Logic/Command/Commands/CleanupCommand.cs
@@ -0,0 +1,8 @@
+﻿using BETBOB.Logic.Standards;
+
+namespace BETBOB.Logic.Command;
+
+public class CleanupCommand : ICommand
+{
+    public void Execute() => Directory.Delete(ProgramStandards.TemporaryFolderLocation(), true);
+}
--- BETBOB.Logic/Standards/ProgramStandards.cs
+++ BETBOB.Logic/Standards/ProgramStandards.cs
@@ -5,11 +5,17 @@ internal static class ProgramStandards
     internal const string ProgramName = "BETBOB";
     internal const string DefaultConfigurationFileName = $"{ProgramName}.config";
 
-    private const string ZipTmpFolder = "tmp";
-    internal static string ZipArchiveTemporaryFolderName(string subfolderName)
+    private const string TmpFolderName = "tmp";
+
+    internal static string TemporaryFolderLocation()
     {
         var appdataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        var subfolder = Path.Join(appdataFolder, ProgramName, ZipTmpFolder, subfolderName);
+        return Path.Join(appdataFolder, ProgramName, TmpFolderName);
+    }
+
+    internal static string ZipArchiveTemporaryFolderName(string subfolderName)
+    {
+        var subfolder = Path.Join(TemporaryFolderLocation(), subfolderName);
 
         SystemsStandards.CreateFolderIfNotExists(subfolder);
         return subfolder;
